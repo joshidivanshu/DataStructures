0966a1c83f1a23ae5c4b9048955893c7a490eb8b
joshidivanshu <joshidivanshu16@gmail.com>
2020-06-15 19:45:37 +0530

    Children Sum Property

---
+++ Trees/ChildrenSumProperty.java
@@ -0,0 +1,22 @@
+class ChildrenSumProperty
+{
+	//ChildSum Property is sum of the leftchild and rightchild should be less than
+	//or equal to the root else if only one child is present then it's value should be less than or equal
+	//to the root.
+	public static boolean isTrue(Node root)
+	{
+		if(root == null)
+			return true;
+		if(root.left == null && root.right == null)
+			return true;
+		int sum = 0;
+		if(root.left != null)
+			sum += root.left.data;
+		if(root.right != null)
+			sum += root.right.data;
+		return (root.data == sum && 
+				isTrue(root.left),
+				isTrue(root.right));
+
+	}
+}
