96430e090962fe2fc3c552f3181ff70a3901abd1
joshidivanshu <joshidivanshu16@gmail.com>
2020-06-15 18:17:27 +0530

    tree functions

--- Trees/TreeTraversals.java
+++ Trees/TreeTraversals.java
@@ -19,4 +19,13 @@ class TreeTraversals
 		preOrder(head.left);
 		preOrder(head.right);
 	}
+	//postorder traversal : left subtree-> rightsubtree -> root
+	public static Node postOrder(Node head)
+	{
+		if(head == null)
+			return null
+		preOrder(head.left);
+		preOrder(head.right);
+		System.out.print(head.key+" ");
+	}
 }
