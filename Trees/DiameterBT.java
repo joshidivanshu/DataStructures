a54d7408fc49ed06eb6f5d60fec34f0cff1db5ac
joshidivanshu <joshidivanshu16@gmail.com>
2020-06-16 14:35:01 +0530

    Diameter of BTree

--- Trees/DiameterBT.java
+++ Trees/DiameterBT.java
@@ -1,4 +1,21 @@
 class DiameterBT
 {
 	//defined as the nnumber of nodes on the longest path between two leaf nodes.
+	//naive soln
+	//find max(1+lh+rh)
+	public static int height(Node head)
+	{
+		if(head == null)
+			return 0;
+		return 1+Math.max(height(head.left),height(head.right));
+	}
+	public static int diameter(Node head)
+	{
+		if(head == null)
+			return 0;
+		int d1 = 1+height(head.left)+height(head.right);
+		int d2 = diameter(head.left);
+		int d3 = diameter(head.right);
+		return Math.max(Math.max(d1,d2),d3);
+	}
 }
