0e68c2e556e08bb8d853513c47864c70f2da1450
joshidivanshu <joshidivanshu16@gmail.com>
2020-06-16 16:27:12 +0530

    Vertical Order

---
+++ Trees/VerticalTraversal.java
@@ -0,0 +1,49 @@
+class VerticalTraversal
+{
+	//we make use of the horizontal distance from the root node.
+	//towards the right direction we mark horizontal distance as positive.
+	//towards the left direction we mark it as negative.
+	//We store this in a treemap and do level order traversal to store these in the treemap.
+	class Pair
+	{
+		Node node;
+		int hd;
+		Pair(Node n,int h)
+		{
+			node = n;
+			hd = h;
+		}
+	}
+	public static void VerticalTraversal(Node head)
+	{
+		Queue<Pair> q = new LinkedList<>();
+		TreeMap<Integer,ArrayList<Integer>> mp = new TreeMap<Integer,ArrayList<Integer>>();
+		q.add(new Pair(head,0));
+		while(!q.isEmpty())
+		{
+			Pair p = q.poll();
+			Node curr = p.node;
+			int hd = p.hd;
+			if(mp.containsKey(hd))
+				mp.get(hd).add(curr.data);
+			else
+			{
+				ArrayList<Integer> al = new ArrayList<>();
+				al.add(curr.data);
+				mp.put(hd,al);
+			}
+			if(curr.left != null)
+				q.add(new Pair(head.left,hd-1));
+			if(curr.right != null)
+				q.add(new Pair(head.right,hd+1));
+		}
+		//print map contents line by line.
+		for(Map.Entry<Integer,ArrayList<Integer>> arr : mp.entrySet())
+		{
+			ArrayList<Integer> temp = arr.getValue();
+			for(int i=0;i<temp.size();i++)
+				System.out.print(arr.get(i)+" ");
+			System.out.println();
+		}
+	}
+}
