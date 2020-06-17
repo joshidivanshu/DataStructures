cbecc3f5ddc4780e4251eacaf225d11a4ca1b15a
joshidivanshu <joshidivanshu16@gmail.com>
2020-06-17 12:51:13 +0530

    BST

---
+++ Trees/Binary Search Trees/BinarySearchTree.java
@@ -0,0 +1,10 @@
+class BinarySearchTree
+{
+	/*
+	1.for every node keys on the left-side are smaller and keys on the right-side are greater.
+	2. All the keys are typically considered as Distinct.
+	3. We do not store redundant values in Binary Search Tree.
+	4. Since it is a linked structure , it's not cache friendly.
+	*/
+
+}
