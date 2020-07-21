3fa6de636eda9d7f1e5cbf1bc7dbdb9143de8c93
joshidivanshu <joshidivanshu16@gmail.com>
2020-07-21 20:53:09 +0530

    Insertion

--- Trees/Binary Search Trees/BST.java
+++ Trees/Binary Search Trees/BST.java
@@ -15,14 +15,39 @@ class BST
 	public static TreeNode insert(TreeNode root,int key)
 	{
 		if(root == null)
-			root = new TreeNode(key);
-		if(root.data == key)
-			return root;
+		{
+			return new TreeNode(key);
+		}
 		else if(root.data < key)
 			root.right = insert(root.right,key);
 		else
 			root.left = insert(root.left,key);
-
-
+		return root;
+	}
+	//iterative insert BST
+	public static TreeNode insert(TreNode root,int key)
+	{
+		TreeNode temp = new TreeNode(key);
+		TreNode parent = root,curr = root;
+		while(curr != null)
+		{
+			parent = curr;
+			if(curr.data > key)
+			{
+				curr = curr.left;
+			}
+			else if(curr.data < key)
+			{
+				curr = curr.right;
+			}
+			else
+		     	return root;
+		}
+		if(parent == null)
+			return temp;
+		if(parent.data > x)
+			parent.left = temp;
+		else
+			parent.right = temp;
 	}
 }
